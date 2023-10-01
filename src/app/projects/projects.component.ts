6c6c860d3a7ebff435203207e4ab7b9efb5e1f02
HarDep <haroldralvaradol@gmail.com>
2023-10-01 15:35:28 -0500

    update projects comp

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,11 +11,27 @@ export class ProjectsComponent implements OnInit {
   repos : any[] = [];
   java : string = 'Java'
   type : string = 'TypeScript'
+  tecs:string[] = ['Spring','Angular','React','Docker','Ionic','Bootstrap'];
+
+  isCopied:boolean = false;
 
   constructor(private gitHubService: GitProjectsService){}
 
   ngOnInit(): void {
     this.repos = this.gitHubService.foundRepos();
+  }
+
+  containsSomeTec(description:string):boolean{
+    return this.tecs.some(word => description.includes(word));
+  }
+
+  copyToClipboard(link:string){
+    navigator.clipboard.writeText(link).then(() => {
+      this.isCopied = true;
+    }).catch((error) => {
+      this.isCopied = false;
+      console.log(error);
+    });
   }
 
 }
