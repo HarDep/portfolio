f3420e871218e71aa1f8d61c4ec18fa5ce9767dc
HarDep <haroldralvaradol@gmail.com>
2023-10-04 00:37:17 -0500

    improve contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -17,12 +17,18 @@ export class ContactComponent {
 
   formMessage:string = '';
 
+  isSent=true;
+
   send(e:Event, ngForm: NgForm){
     e.preventDefault();
     emailjs.sendForm('service_ajy5r8u', 'template_i8nn1ig', e.target as HTMLFormElement, '9-4kKk4M25x5D32fm')
       .then((result: EmailJSResponseStatus) => {
+        if(result.text != 'OK'){
+          this.isSent = false;
+        }
         console.log(result.text);
       }, (error) => {
+        this.isSent = false;
         console.log(error.text);
       });
     this.formName = "";
